9b087b60ca2d46f0b04b6fd490a8c3c9bbaa2240
Cass <114196473+EepyGirlCass@users.noreply.github.com>
2026-07-23 14:17:49 -0400

    start enemy graphics

---
+++ scenes/enemy_sprite.gd
@@ -0,0 +1,24 @@
+extends Sprite3D
+
+var sprite_coords := Vector2i(0, 0)
+var sprite_rect: Rect2i:
+	get: return Rect2i(sprite_coords, Vector2i(128, 256))
+
+@onready var camera: Camera3D = $Player/CameraPivot/Camera3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	var h_angle = Vector2(camera.global_position.x, camera.global_position.y).angle_to(Vector2(global_position.x, global_position.y))
+	var yaw = global_rotation.y
+	var h_visual_angle = h_angle - yaw
+	# 0 to 2PI, 0 to 8
+	h_visual_angle *= (8/TAU)
+	# 0 to 8, -4 to 4
+	h_visual_angle -= 4
+	# snap to nearest
+	h_visual_angle = roundi(h_visual_angle)
