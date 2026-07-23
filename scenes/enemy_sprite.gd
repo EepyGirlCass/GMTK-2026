e28a23a4d499b5924fc82ff471a402e90a870c11
Cass <114196473+EepyGirlCass@users.noreply.github.com>
2026-07-23 16:08:48 -0400

    fr tho

--- scenes/enemy_sprite.gd
+++ scenes/enemy_sprite.gd
@@ -2,7 +2,7 @@ extends Sprite3D
 
 var sprite_coords := Vector2i(0, 0)
 var sprite_rect: Rect2i:
-	get: return Rect2i(sprite_coords, Vector2i(128, 256))
+	get: return Rect2i(Vector2i(sprite_coords.x * 128, sprite_coords.y * 256), Vector2i(128, 256))
 
 
 @onready var player: Player = $"../../../Player"
@@ -16,18 +16,28 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	var h_mirror = false
+	sprite_coords = Vector2i.ZERO
+	
+	flip_h = false
 	var h_angle = Vector2(camera_position.x, camera_position.z).angle_to_point(Vector2(global_position.x, global_position.z))
 	var yaw = global_rotation.y - PI * 0.5
 	var h_visual_angle = fmod(h_angle + yaw + PI + 16 * TAU, TAU) - PI
 	# -PI to PI, 4 to -4
-	h_visual_angle *= (-8/TAU)
+	h_visual_angle *= (-4/PI)
 	# snap to nearest
 	h_visual_angle = roundi(h_visual_angle)
 	if h_visual_angle < 0:
-		h_mirror = true
+		flip_h = true
 		h_visual_angle *= -1
-	sprite_coords = Vector2i(h_visual_angle * 128, 512)
+	sprite_coords.x = h_visual_angle
+	
+	var h_dist = Vector2(camera_position.x, camera_position.z).distance_to(Vector2(global_position.x, global_position.z))
+	var v_angle = atan((camera_position.y - global_position.y)/ h_dist)
+	# -PI/2 to PI/2, -3.5 to 3.5
+	v_angle *= (-7/PI)
+	v_angle += 2.5
+	v_angle = roundi(v_angle)
+	v_angle = clampi(v_angle, 0, 5)
+	sprite_coords.y = v_angle
 	
 	region_rect = sprite_rect
-	flip_h = h_mirror
