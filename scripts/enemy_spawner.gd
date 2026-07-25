c6c13acf0c7e24609e99631d5ef5028eb78e256d
MayaMogus <damienmahony.dm@gmail.com>
2026-07-25 21:51:10 +0100

    level

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -9,10 +9,13 @@ var enemy_types : Array = [
 	Ranger
 ]
 
+var max_enemies : int = 25
+
 func _process(_delta: float) -> void:
 	if GameTime.time >= next_wave_time: spawn_wave()
 	pass
 func spawn_wave():
+	if enemies.get_child_count() > max_enemies: return
 	next_wave_time = GameTime.time + spawn_wave_time
 	for node3d : Node3D in get_children():
 		spawn_enemy(enemy_types.pick_random(), node3d.global_position)
